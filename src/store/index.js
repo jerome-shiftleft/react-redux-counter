bf241438cc169c8f34cf5f695910fdc41f0ee8c1
jerome-shiftleft <jerome@theshiftleft.com>
2024-03-07 05:30:50 +0800

    progress

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import { createStore } from "redux";
+
+const counterReducer = (state = {counter: 0}, action) => {
+  if (action.type === 'increment') {
+    return {
+      counter: state.counter + 1
+    }
+  }
+
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter - 1
+    }
+  }
+
+  return state;
+} // end of const counterReducer = (state = {counter: 0}, action)
+
+const store = createStore(counterReducer);
+
+export default store;

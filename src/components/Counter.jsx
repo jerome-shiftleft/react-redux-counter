df809111b7e0961ed24ac2d2ba63cae585f8c7b8
jerome-shiftleft <jerome@theshiftleft.com>
2024-03-07 06:40:42 +0800

    progress

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,6 +4,7 @@ import classes from "./Counter.module.css";
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const showCounter = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
     dispatch({ type: "increment" });
@@ -11,18 +12,27 @@ const Counter = () => {
 
   const increaseHandler = (amount) => {
     dispatch({ type: "increase", amount });
-  }
+  };
 
   const decrementHandler = () => {
     dispatch({ type: "decrement" });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      <div
+        className={classes.value}
+        style={{
+          visibility: showCounter ? "visible" : "hidden",
+        }}
+      >
+        {counter}
+      </div>
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={() => increaseHandler(5)}>Increase by 5</button>
